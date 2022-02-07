1b172a4f5694ec01a99733d891baf05795c89746
Grek046 <Dmitriy89207173111@yandex.ru>
2022-02-07 17:41:48 +0300

    Внёс изменения в файл test.
    Программа открывает первые 10 объявлений и парсит информацию

--- Code.py
+++ Code.py
@@ -1,6 +1,6 @@
-# import requests
+import requests
 import time
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup as bs
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -13,19 +13,58 @@ link = "https://www.avito.ru/rossiya/"
 
 driver = webdriver.Chrome()
 driver.maximize_window()
-driver.get(link)
+page = driver.get(link)
 
 #имитируем ввод нашего запроса в поисковике авито
 driver.find_element(By.TAG_NAME, "label").send_keys(zapros + Keys.ENTER)
 
 time.sleep(5)
 
-soup = BeautifulSoup(driver.page_source, 'lxml')
-ads = soup.findAll('div',class_='iva-item-titleStep-pdebR')
-for ad in ads:
-    name = ad.find('h3', class_='title-root-zZCwT').text.strip()
-    rezult.append(name)
-print(rezult[0:10])
+ads = driver.find_elements(By.CLASS_NAME, "iva-item-content-rejJg")
+urls = []
+for ad in ads[0:3]:
+    a = ad.get_attribute('href')
+    url = {
+        'href': a
+    }
+    urls.append(url)
+    print(urls)
+
+# name = driver.find_element(By.CLASS_NAME, 'title-root-zZCwT').text
+# price = driver.find_element(By.CLASS_NAME, 'price-text-_YGDY').text
+# id = driver.find_element(By.ID, 'i2316124193').text
+# print(name)
+# print(price)
+# print(id)
+
+# ob = driver.find_element(By.CLASS_NAME, 'iva-item-priceStep-uq2CQ')
+# ob.click() #открываем объявление
+# # НУЖНО ПОНЯТЬ КАК СЧИТАТЬ НОВУЮ СТРАНИЦУ И ПАРСИТЬ С НЕЁ
+# soup = BeautifulSoup(driver.page_source, 'lxml')
+# name = soup.find('span', class_='title-info-title-text')
+# print(name)
+# print(id)
+
+# soup = BeautifulSoup(driver.page_source, 'lxml')
+# ads = soup.findAll('div',class_='iva-item-titleStep-pdebR')
+# for ad in ads[0:3]:
+#     name = ad.find('h3', class_='title-root-zZCwT').text.strip()
+#     # ob = driver.find_element(By.CSS_SELECTOR, '#i2258432180 > div > div.iva-item-aside-GOesg')
+#     ob = driver.find_element(By.CLASS_NAME, 'iva-item-priceStep-uq2CQ')
+#     ob.click() #открываем объявление
+#     st = BeautifulSoup(driver.page_source, 'lxml')
+#     elements = st.findAll('div',class_='item-view-search-info-redesign')
+#     for el in elements:
+#         # id = el.find('snap')
+#         id = driver.find_element(By.TAG_NAME, 'span').text
+#
+#     # id = st.find('snap', class_='js-item-price')
+#     # ss = driver.find_element(By.LINK_TEXT, name).get_attribute('href') #ссылка на каждое объявление
+#     rezult.append([name,id])
+#     # rezult.append(id)
+#     # rezult.append(name)
+#     # rezult.append(ss)
+# print(rezult)
 
 
 
--- test.py
+++ test.py
@@ -1,10 +1,42 @@
+import requests
+import time
+from bs4 import BeautifulSoup as bs
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-driver = webdriver.Firefox()
 
-    # Navigate to url
-driver.get("http://www.google.com")
 
-    # Enter "webdriver" text and perform "ENTER" keyboard action
-driver.find_element(By.NAME, "q").send_keys("webdriver" + Keys.ENTER)
+zapros = input('Введите запрос - ')
+
+link = "https://www.avito.ru/rossiya/"
+
+driver = webdriver.Chrome()
+driver.maximize_window()
+page = driver.get(link)
+
+driver.find_element(By.TAG_NAME, "label").send_keys(zapros + Keys.ENTER)
+time.sleep(10)
+
+ads = driver.find_elements(By.CLASS_NAME, "iva-item-titleStep-pdebR")
+urls = []
+for ad in ads[0:10]:
+    name = ad.find_element(By.CLASS_NAME, 'title-root-zZCwT').text.strip()
+    url_ad = ad.find_element(By.LINK_TEXT, name).get_attribute('href')
+    links = {
+        'href': url_ad
+    }
+    urls.append(links)
+for url in urls:
+    driver.get(url['href'])
+    id = driver.find_element(By.XPATH, '/html/body/div[3]/div[1]/div[2]/div[5]/div[2]/div[1]/div/div[5]/span').text
+    price = driver.find_element(By.CLASS_NAME, 'js-item-price').text
+    date = driver.find_element(By.CLASS_NAME, 'title-info-metadata-item-redesign').text
+    author = driver.find_element(By.XPATH, '/html/body/div[3]/div[1]/div[2]/div[5]/div[2]/div[1]/div/div[4]/div/div/div[1]/div[1]/div/a').text
+    address = driver.find_element(By.CLASS_NAME, 'item-address-georeferences').text
+    views = driver.find_element(By.CLASS_NAME, 'title-info-metadata-item').text
+    print(id)
+    print(price)
+    print(date)
+    print(author)
+    print(address)
+    print(views)
